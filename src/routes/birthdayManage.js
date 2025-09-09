2ae0cbab7e6975c50dca3dd128ab3d34dbdde6ab
danielfernandez <danielfernandez@craftelier.com>
2025-09-09 15:38:26 +0200

    imgs cabecera

--- src/routes/birthdayManage.js
+++ src/routes/birthdayManage.js
@@ -8,7 +8,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 router.get('/', birthdayController.getBirthdays);
 router.get('/getAllbirthday', birthdayController.getAllBirthdays);
 router.post('/', upload.single('photo'), createBirthdayValidators, birthdayController.createBirthday);
-router.put('/:id', createBirthdayValidators, createBirthdayValidators.updatePlayer);
+router.put('/:id', createBirthdayValidators, birthdayController.updatePlayer);
 
 
 module.exports = router;
